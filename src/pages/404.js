0646675ec2054c652c46d8de035101edf266e718
Berts <romareda@gmail.com>
2021-04-08 00:12:49 +0200

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -8,7 +8,7 @@ import DocsTitle from "../components/docs-title"
 const NotFoundPage = () => (
   <>
     <SEO title="Not found"/>
-    <h1>Not found</h1>
+    <h1> Error Not found</h1>
     <p>Unfortunately, the page you requested cannot be found.</p>
     <p><AnchorLink href="/">Go to <DocsTitle/> docs home</AnchorLink></p>
   </>
